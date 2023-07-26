cbc19785ee3006d36b69f959a207a305695afeb4
Saad Irfan <105337416+saadirfan370@users.noreply.github.com>
2023-07-26 05:17:41 -0700

    slider

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './componrnts/Header/Header';
 import Hero from './componrnts/Hero/Hero';
 import Companies from './componrnts/Companies/Companies';
+import Residencies from './componrnts/Residencies/Residencies';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Hero />
       </div>
       <Companies />
+      <Residencies />
     </div>
   );
 }
---
+++ src/componrnts/Residencies/Residencies.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "./Residencies.css";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import "swiper/css";
+import data from "../../utils/slider.json";
+import { sliderSetting } from "../../utils/common";
+
+const Residencies = () => {
+  return (
+    <section className="r-wrapper">
+      <div className="paddings innerWidth r_container">
+        <div className="r_head flexColStart">
+          <span className="orangeText">Best Choices</span>
+          <span className="primaryText">Popular Residencies</span>
+        </div>
+
+        <Swiper {...sliderSetting}>
+          <SliferButtons />
+          {data.map((card, i) => (
+            <SwiperSlide key={i}>
+              <div className="flexColStart r_card">
+                <img src={card.image} alt="home" />
+
+                <span className="secondaryText r_price">
+                  <span style={{ color: "orange" }}>$</span>
+                  <span>{card.price}</span>
+                </span>
+                <span className="primaryText">{card.name}</span>
+                <span className="secondaryText">{card.detail}</span>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
+  );
+};
+
+export default Residencies;
+
+const SliferButtons = () => {
+  const swiper = useSwiper();
+
+  return (
+    <div className="flexCenter r_button">
+      <button onClick={() => swiper.slidePrev()}>&lt;</button>
+      <button onClick={()=> swiper.slideNext()}>&gt;</button>
+    </div>
+  );
+};
